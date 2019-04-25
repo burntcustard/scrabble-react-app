9ba477d7cb59431dbbbfc7602c702178a4872ed4
burntcustard <burntcustard@gmail.com>
2019-04-25 15:01:47 +0100

    Don't have separate state in input components

--- src/components/InputPlayerName.js
+++ src/components/InputPlayerName.js
@@ -2,20 +2,14 @@ import React, { useState } from 'react';
 
 
 function InputPlayerName({ player, index, onChange }) {
-  const [text, setField] = useState(player);
-
-  function update(event) {
-    setField(event.target.value);
-    onChange(event.target.value);
-  }
 
   return (
     <input
       className="player-name"
       aria-label={`Player ${index + 1} name`}
       type="text"
-      value={text}
-      onChange={update}
+      value={player}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 }
--- src/components/InputScore.js
+++ src/components/InputScore.js
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
 
 
-function InputScore({ player, index, onChange }) {
-  const [text, setField] = useState('');
-
-  function update(event) {
-    setField(event.target.value);
-    onChange(event.target.value);
-  }
+function InputScore({ player, value, index, onChange }) {
 
   return (
     <input
       className="score"
       aria-label={player + ", running total " + (index + 1)}
       type="tel"  // To use the numpad onscreen keyboard
-      value={text}
-      onChange={update}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 }
